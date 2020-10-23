8bea7cb242b3d64b9b78b57e1859a235f9ed3e8b
Maris Sekar <maris.sekar@gmail.com>
2020-10-23 17:43:50 -0600

    Updated.

--- code.R
+++ code.R
@@ -18,3 +18,37 @@ file.copy(fullpath, getwd())
 
 # check if the file exists
 file.exists(filename)
+
+# readr and readxl
+library(dslabs)
+library(tidyverse)    # includes readr
+library(readxl)
+
+# inspect the first 3 lines
+read_lines("murders.csv", n_max = 3)
+
+# read file in CSV format
+dat <- read_csv(filename)
+
+#read using full path
+dat <- read_csv(fullpath)
+head(dat)
+
+#Ex：
+path <- system.file("extdata", package = "dslabs")
+files <- list.files(path)
+files
+
+filename <- "murders.csv"
+filename1 <- "life-expectancy-and-fertility-two-countries-example.csv"
+filename2 <- "fertility-two-countries-example.csv"
+dat=read.csv(file.path(path, filename))
+dat1=read.csv(file.path(path, filename1))
+dat2=read.csv(file.path(path, filename2))
+
+#check class
+# filename is defined in the previous video
+# read.csv converts strings to factors
+dat2 <- read.csv(filename)
+class(dat2$abb)
+class(dat2$region)
